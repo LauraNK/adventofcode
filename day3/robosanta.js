40f7a937e7840048ee385f136de64440db0ba70b
Laura <laura.kyle91@gmail.com>
2015-12-03 14:01:46 -0500

    day 3 challenge 2

---
+++ day3/robosanta.js
@@ -0,0 +1,43 @@
+// http://adventofcode.com/day/3 
+
+var input = '>^^v^<>v<<<v<'; 
+var count = 0;
+var santa = {
+  x: 0,
+  y: 0
+};
+var robo = {
+  x: 0,
+  y: 0
+};
+var houses = [];
+var who;
+
+function getUnique() {
+  for (var i = 0; i < input.length; i++) {
+    who = i % 2 ? robo : santa;
+
+    switch (input[i]) {
+
+      case '^': who.x++;
+        break;
+      case 'v': who.x--;
+        break;
+      case '>': who.y++;
+        break;
+      case '<': who.y--;
+        break;
+    }
+
+    var coord = who.x + 'x' + who.y;
+
+
+    if (houses.indexOf(coord) == -1) {
+      houses.push(coord);
+      count++;
+    }
+
+  }
+  console.log(count);
+}
+getUnique();
