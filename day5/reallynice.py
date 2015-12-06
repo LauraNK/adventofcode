6fe403f009ec4c138c2ef54149ebeba2ea08c13a
Laura <laura.kyle91@gmail.com>
2015-12-06 14:02:11 -0500

    day 5 challenge 2

---
+++ day5/reallynice.py
@@ -0,0 +1,23 @@
+f = open('input.txt', 'r')
+input = f.read()
+f.close()
+
+input = input.split('\n')
+niceCount = 0
+
+for i in input:
+    
+     # It contains at least one letter which repeats with exactly one letter between them
+    for char in range(0, len(i)-2):
+        if i[char] == i[char+2]:
+            break
+    else:
+        continue
+            
+        # Contains a pair of any two letters that appears at least twice in the string without overlapping   
+    for char in range(0, len(i)-1):
+        if i.count(i[char] + i[char+1]) > 1:
+            niceCount +=1
+            break
+            
+print(niceCount)
